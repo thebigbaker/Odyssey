a5db0d31a87724014b1cef94e6f760fb3aaadbbe
Jeremy Baker <30672714+thebigbaker@users.noreply.github.com>
2020-03-29 17:33:24 -0600

    Testimonals Add

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      fullWidth:  true,
+      indicators:  true
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
+  });
